fe638f9a5bbaa5cfbf1438ac0c265f8e9834228c
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-01-28 22:52:18 +0545

    initialize GameStore.Api project with basic configuration and settings

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,25 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.Run();
+
